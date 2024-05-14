e02f507b28d4d7dd6ece5f2ef3b3150a85599dc8
Sakif Surur <sakif.surur@blackbird.cloud>
2024-05-14 11:51:26 +0200

    chore: add example on docs

--- README.md
+++ README.md
@@ -5,7 +5,11 @@ A Terraform module which configures your [AWS Backup](https://aws.amazon.com/bac
 
 ## Example
 ```hcl
-
+module "backup" {
+  source                              = "../"
+  name                                = "centralized-backup"
+  kms_key_arn                         = "arn:aws:kms:us-west-2:xxxxx:key/xxxxxxxxxxxxx"
+}
 ```
 
 ## Requirements
--- example/main.tf
+++ example/main.tf
@@ -1,5 +1,6 @@
 module "backup" {
-  source                              = "../"
-  name                                = "centralized-backup"
-  kms_key_arn                         = "arn:aws:kms:us-west-2:xxxxx:key/xxxxxxxxxxxxx"
+  source      = "blackbird-cloud/backup/aws"
+  version     = "~> 1"
+  name        = "centralized-backup"
+  kms_key_arn = "arn:aws:kms:us-west-2:xxxxx:key/xxxxxxxxxxxxx"
 }
